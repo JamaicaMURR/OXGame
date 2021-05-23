bc4759b258cf9683a59454390a199bd581c357e0
murr123yandex.ru <murr123yandex.ru@outlook.com>
2021-05-24 01:38:57 +0300

    Sounds for buttons in menu added

--- Assets/Scripts/ButtonMouseEnterSound.cs
+++ Assets/Scripts/ButtonMouseEnterSound.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonSound : MonoBehaviour
+public class ButtonMouseEnterSound : MonoBehaviour
 {
     public AudioSource audioSource;
     public AudioClip sound;
